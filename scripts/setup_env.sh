d5f86e2537e9fe21e010a39fb96f19d420a17c86
Emmanuel Leroy <emmanuel.leroy@oracle.com>
2021-02-25 19:05:32 -0800

    password policy issue

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -3,11 +3,11 @@
 PLATFORM=$(uname)
 
 which sql \
-|| unzip sqlcl-*.zip -d \
-    && export PATH=$PATH:./sqlcl/bin/)
+|| (unzip scripts/sqlcl-*.zip -d ./ \
+    && export PATH=$PATH:./sqlcl/bin/ )
 
-TERRAFORM_VERSION=0.14.6
-which terraform \
-|| (curl -sLO https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_${PLATFORM,,}_amd64.zip
-    && unzip terraform_${TERRAFORM_VERSION}_${PLATFORM,,}_amd64.zip \
-    && mv terraform /usr/local/bin)
+# TERRAFORM_VERSION=0.14.6
+# which terraform \
+# || (curl -sLO https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_${PLATFORM,,}_amd64.zip
+#     && unzip terraform_${TERRAFORM_VERSION}_${PLATFORM,,}_amd64.zip \
+#     && mv terraform /usr/local/bin)
--- terraform/creds.tf
+++ terraform/creds.tf
@@ -1,34 +1,49 @@
 resource "random_password" "admin_password" {
     count = length(var.databases)
     length           = 28
-    special          = true
-    override_special = "#_"
+    special          = false
+    min_special      = 1
+    min_upper        = 1
+    min_lower        = 1
+    min_numeric      = 1 
 }
 
 resource "random_password" "apex_admin_password" {
     count = length(var.databases)
     length           = 28
-    special          = true
-    override_special = "#_"
+    special          = false
+    min_special      = 1
+    min_upper        = 1
+    min_lower        = 1
+    min_numeric      = 1 
 }
 
 resource "random_password" "schema_password" {
     count = length(keys(var.environments))
     length           = 28
-    special          = true
-    override_special = "#_"
+    special          = false
+    min_special      = 1
+    min_upper        = 1
+    min_lower        = 1
+    min_numeric      = 1 
 }
 
 resource "random_password" "ws_password" {
     count = length(keys(var.environments))
     length           = 28
-    special          = true
-    override_special = "#_"
+    special          = false
+    min_special      = 1
+    min_upper        = 1
+    min_lower        = 1
+    min_numeric      = 1 
 }
 
 resource "random_password" "ws_admin_password" {
     count = length(keys(var.environments))
     length           = 28
-    special          = true
-    override_special = "#_"
+    special          = false
+    min_special      = 1
+    min_upper        = 1
+    min_lower        = 1
+    min_numeric      = 1 
 }
