54b9f298f634c611f6bb8ff7ec48fc84276fcf63
Emmanuel Leroy <emmanuel.leroy@oracle.com>
2021-03-02 08:00:26 -0800

    Revert "support table changelog"
    This reverts commit 681bdeff8d72bead61ca477885eea80a6a5e9748.

--- scripts/sql/change_tracking/gen_data_change.sql
+++
@@ -1,5 +0,0 @@
--- Copyright © 2021, Oracle and/or its affiliates. 
--- All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
-
-cd changelogs;
-lb data -filter include -object &1;
